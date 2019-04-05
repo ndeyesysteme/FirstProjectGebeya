900004414ab86594bad27ff4a2893362116e055c
diop ndeye ami <diopndeyeamie@gmail.com>
2019-04-05 11:40:36 +0000

    initialising project

---
+++ src/main/java/com/example/FirstProjectGebeya/FirstProjectGebeyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstProjectGebeya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectGebeyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectGebeyaApplication.class, args);
+	}
+
+}
